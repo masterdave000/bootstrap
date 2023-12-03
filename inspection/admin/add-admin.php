68035d6cd78942c41519f6f3b881191cc0fd7b8d
masterdave000 <127076290+masterdave000@users.noreply.github.com>
2023-12-03 08:31:49 +0800

    Delete: Two Bottom <div></div>

--- inspection/admin/add-admin.php
+++ inspection/admin/add-admin.php
@@ -93,12 +93,6 @@ $fullname = $_SESSION['fullname'];
 </div>
 <!-- End of Main Content -->
 
-</div>
-<!-- End of Content Wrapper -->
-
-</div>
-<!-- End of Page Wrapper -->
-
 <!-- Scroll to Top Button-->
 <a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
