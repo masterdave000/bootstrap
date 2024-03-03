253af35c9695dc7a252e1bd46e455292fd2b007f
masterdave000 <127076290+masterdave000@users.noreply.github.com>
2024-03-03 14:41:50 +0800

    Edit: success -> danger

--- login-check.php
+++ login-check.php
@@ -1,7 +1,7 @@
 <?php
 if (!isset($_SESSION['user_id'])) {
 	$_SESSION['error_login'] = "
-	<div class='msgalert alert--success' id='alert'>
+	<div class='msgalert alert--danger' id='alert'>
 		<div class='alert__message'>
 			Sign in Failed
 		</div>
