f3f5e1f9e487076aa4f56164736f7d6a67d55144
masterdave000 <127076290+masterdave000@users.noreply.github.com>
2024-03-08 17:38:32 +0800

    Edit: if(){} -> if(): endif;

--- inspection/includes/footer.php
+++ inspection/includes/footer.php
@@ -10,25 +10,24 @@
 
 
 <?php
-if ($title === 'Dashboard') {
-    echo '
-    <!-- Page level plugins -->
-    <script src="<?php echo SITEURL ?>vendor/chart.js/Chart.min.js"></script>
-
+if ($title === 'Dashboard') :?>
+<!-- Page level plugins -->
+<script src="<?php echo SITEURL ?>vendor/chart.js/Chart.min.js"></script>
 <!-- Page level custom scripts -->
 <script src="<?php echo SITEURL ?>assets/js/demo/chart-area-demo.js"></script>
-<script src="<?php echo SITEURL ?>assets/js/demo/chart-pie-demo.js"></script>';
-} elseif ($title === 'Manage Admin') {
-echo '
+<script src="<?php echo SITEURL ?>assets/js/demo/chart-pie-demo.js"></script>
+
+<?php elseif ($title === 'Manage Admin'): ?>
+
 <!-- Page level plugins -->
 <script src="<?php echo SITEURL ?>vendor/datatables/jquery.dataTables.min.js"></script>
 <script src="<?php echo SITEURL ?>vendor/datatables/dataTables.bootstrap4.min.js"></script>
-
-<!-- Page level custom scripts -->
+<!--Page level custom scripts -->
 <script src="<?php echo SITEURL ?>asets/js/demo/datatables-demo.js"></script>
-';
 
-}
+<?php endif;?>
+
+
 
 
 
