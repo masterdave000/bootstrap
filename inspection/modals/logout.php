ae9cf5e64b7129167b339e02448fefe06235eb26
masterdave000 <127076290+masterdave000@users.noreply.github.com>
2024-03-08 20:58:29 +0800

    Edit: Button Color

--- inspection/modals/logout.php
+++ inspection/modals/logout.php
@@ -11,8 +11,8 @@
             </div>
             <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
             <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-primary" href="./../../logout.php">Logout</a>
+                <button class="btn btn-dark" type="button" data-dismiss="modal">Cancel</button>
+                <a class="btn btn-danger" href="./../../logout.php">Logout</a>
             </div>
         </div>
     </div>
