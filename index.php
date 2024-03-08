5c41859de176ca6271d629a37764166d53f9b13d
masterdave000 <127076290+masterdave000@users.noreply.github.com>
2024-03-08 19:46:13 +0800

    Add: Favicon

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ require "./config/constants.php";
     <title>OBOS - Login</title>
 
     <!-- Custom fonts for this template-->
+    <link rel="icon" type="image/x-icon" href="<?php echo SITEURL?>assets/img/lgu_logo.ico">
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
