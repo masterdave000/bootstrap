398a5d8b4b64323e15eb589720aa6dd9ac84835e
masterdave000 <127076290+masterdave000@users.noreply.github.com>
2024-03-11 00:01:28 +0800

    Edit: Update Table Relatioships

--- assets/database/inspection.sql
+++ assets/database/inspection.sql
@@ -1,11 +1,4 @@
-CREATE TABLE
-    admin ( 
-        admin_id int NOT NULL AUTO_INCREMENT,
-        admin_name varchar(100) NOT NULL,
-        PRIMARY KEY(admin_id)
-    );
-
-CREATE TABLE
+ CREATE TABLE
     violation (
         violation_id int NOT NULL AUTO_INCREMENT,
         findings varchar(100) NOT NULL,
@@ -28,32 +21,31 @@ CREATE TABLE
         owner_id int NOT NULL,
         bus_name varchar(100) NOT NULL,
         bus_address varchar(100) NOT NULL,
-        contact_no int NOT NULL,
+        bus_type varchar(50) DEFAULT NULL,
+        bus_contact_number varchar(11) NOT NULL,
         floor_area double DEFAULT NULL,
-        singage_area double DEFAULT NULL,
+        signage_area double DEFAULT NULL,
+        bus_img_url varchar(50) DEFAULT 'no-image.png',
         PRIMARY KEY(bus_id),
-        FOREIGN KEY(owner_id) REFERENCES owner(owner_id),
-        FOREIGN KEY(location_id) REFERENCES location(location_id)
+        FOREIGN KEY(owner_id) REFERENCES owner(owner_id)
     ); 
- 
-CREATE TABLE
-    category_list (
+
+
+CREATE TABLE category_list (
         category_id int NOT NULL AUTO_INCREMENT,
         category_name varchar(100) NOT NULL,
         PRIMARY KEY(category_id)
-    );
+);
     
-CREATE TABLE
-    item_list (
+CREATE TABLE item_list (
         item_id int NOT NULL AUTO_INCREMENT,
         category_id int NOT NULL,        
-        equipment_name varchar(100) NOT NULL,
-        PRIMARY KEY(equipment_id),
+        item_name varchar(100) NOT NULL,
+        PRIMARY KEY(item_id),
         FOREIGN KEY(category_id) REFERENCES category_list(category_id)
-    );
+);
     
-CREATE TABLE
-    inspection ( 
+CREATE TABLE inspection ( 
         inspection_id int NOT NULL AUTO_INCREMENT,
         bus_id int NOT NULL,
         application_type varchar(50) NOT NULL DEFAULT 'Annual',
@@ -66,7 +58,7 @@ CREATE TABLE
         time_received time NOT NULL,
         PRIMARY KEY(inspection_id),
         FOREIGN KEY(bus_id) REFERENCES business(bus_id)
-    );
+);
     
 CREATE TABLE item_inspection (
 	item_id int NOT NULL,
@@ -75,12 +67,16 @@ CREATE TABLE item_inspection (
     FOREIGN KEY (inspection_id) REFERENCES inspection(inspection_id)
 );
     
-CREATE TABLE
-    inspector ( 
+CREATE TABLE inspector ( 
         inspector_id int NOT NULL AUTO_INCREMENT,
-        inspector_name varchar(100) NOT NULL,
+        inspector_firstname varchar(100) NOT NULL,
+        inspector_midname varchar(100) DEFAULT NULL,
+        inspector_lastname varchar(100) NOT NULL,
+        inspector_suffix varchar(100) DEFAULT NULL,
+        contact_number varchar(11) NOT NULL,
+        img_url varchar(100) DEFAULT 'default.png',
         PRIMARY KEY(inspector_id)
-    );
+);
 
 CREATE TABLE inspection_inspector (
 	inspector_id int NOT NULL,
@@ -89,27 +85,23 @@ CREATE TABLE inspection_inspector (
     FOREIGN KEY (inspection_id) REFERENCES inspection (inspection_id)
 );
     
-CREATE TABLE
-    fee (
+CREATE TABLE fee (
         fee_id int NOT NULL AUTO_INCREMENT,
         building_fee decimal(10, 2) NOT NULL,
         plumbing_fee decimal(10, 2) NOT NULL,
         signage_fee decimal(10, 2) NOT NULL,
         PRIMARY KEY(fee_id)
-    );
+);
 
-CREATE TABLE
-    annual_fees ( 
+CREATE TABLE annual_fees ( 
         annualfee_id int NOT NULL AUTO_INCREMENT,
         fee_id int NOT NULL,
         total_fee int NOT NULL,
         PRIMARY KEY(annualfee_id),
         FOREIGN KEY(fee_id) REFERENCES fees(fee_id)
-    );
+);
     
-    
-CREATE TABLE
-    equipment_list_report (
+CREATE TABLE equipment_list_report (
         report_id int NOT NULL AUTO_INCREMENT,
         bus_id int NOT NULL,
         equipment_id int NOT NULL,
@@ -129,9 +121,7 @@ CREATE TABLE
         FOREIGN KEY(inspector_id) REFERENCES inspector(inspector_id)
     );
     
-
-CREATE TABLE
-    certificate (
+CREATE TABLE certificate (
         certificate_id int NOT NULL AUTO_INCREMENT,
         bus_id int NOT NULL,
         annualfee_id int NOT NULL,
@@ -145,8 +135,7 @@ CREATE TABLE
     );
 
     
-CREATE TABLE
-    annual_ins ( 
+CREATE TABLE annual_ins ( 
         annual_id int NOT NULL AUTO_INCREMENT,
         inspector_id int NOT NULL,
         violation_id int NOT NULL,
@@ -159,10 +148,9 @@ CREATE TABLE
         FOREIGN KEY(inspector_id) REFERENCES inspector(inspector_id),
         FOREIGN KEY(violation_id) REFERENCES violation(violation_id),
         FOREIGN KEY(bus_id) REFERENCES business(bus_id)
-    );
+);
     
-CREATE TABLE
-    annual_bus_ins ( 
+CREATE TABLE annual_bus_ins ( 
         annualbl_id int NOT NULL AUTO_INCREMENT,
         inspection_id int NOT NULL,
         violation_id int NOT NULL,
@@ -173,4 +161,25 @@ CREATE TABLE
         FOREIGN KEY(inspection_id) REFERENCES inspection(inspection_id),
         FOREIGN KEY(violation_id) REFERENCES violation(violation_id),
         FOREIGN KEY(annualfee_id) REFERENCES annual_fees(annualfee_id)
-    );
+);
+    
+CREATE TABLE users ( 
+        user_id int NOT NULL AUTO_INCREMENT,
+        inspector_id int DEFAULT NULL,
+        username varchar(100) NOT NULL,
+        password varchar(255) NOT NULL,
+        role varchar(20) NOT NULL,
+        PRIMARY KEY(user_id),
+        FOREIGN KEY (inspector_id) REFERENCES inspector (inspector_id)
+);
+
+CREATE VIEW user_view AS
+SELECT u.user_id, i.inspector_firstname, i.inspector_midname, i.inspector_lastname, i.inspector_suffix, u.username, i.img_url
+FROM users u LEFT JOIN inspector i 
+ON u.inspector_id = i.inspector_id;
+    
+CREATE VIEW business_view AS
+SELECT bus_id, bus_name, bus_address, bus_type, bus.bus_contact_number, floor_area, signage_area, bus_img_url, owner.* FROM business bus
+LEFT JOIN owner ON bus.owner_id = owner.owner_id;
+
+
