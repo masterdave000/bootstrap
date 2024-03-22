fa71988a6faf0d85ec13dac76a964137648c2467
masterdave000 <127076290+masterdave000@users.noreply.github.com>
2024-03-22 16:10:32 +0800

    Edit: Owner -> Inspector

--- inspection/inspector/modals/delete.php
+++ inspection/inspector/modals/delete.php
@@ -4,7 +4,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Delete Owner Profile</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Delete Inspector Profile</h5>
                 <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">×</span>
                 </button>
