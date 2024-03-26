4660299ce4f3f83bffc0d0ccbea3b142a7714209
masterdave000 <127076290+masterdave000@users.noreply.github.com>
2024-03-26 21:42:34 +0800

    Edit: Add Id's to id attribute

--- inspection/inspector/modals/delete.php
+++ inspection/inspector/modals/delete.php
@@ -1,6 +1,6 @@
 <!-- Logout Modal-->
-<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
+<div class="modal fade" id="deleteModal-<?php echo $inspector['inspector_id']?>" tabindex="-1" role="dialog"
+    aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
