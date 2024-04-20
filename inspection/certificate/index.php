43842921ee97c375017bb8df8bddc14915213ff5
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-04-20 12:35:31 +0800

    Add: Certificate of Annual Inspection Structures and Styles

---
+++ inspection/certificate/index.php
@@ -0,0 +1,240 @@
+<?php 
+
+    $title = "Equipment List Certificate";
+    include './../includes/side-header.php';
+
+?>
+
+<div id="content-wrapper">
+    <div class="content">
+        <?php require './../includes/top-header.php' ?>
+
+        <div class="container-fluid d-flex justify-content-center py-5">
+            <div class="annual-container">
+                <div class="annual-sheet">
+                    <img src="./../../assets/img/annual-certificate.jpg" alt="annual-certificate"
+                        class="annual-sheet-image">
+                    <div class="annual-sheet-left">
+                        <div
+                            class="d-flex justify-content-center annual-header annual-business-name mb-2 pb-1 annual-data">
+                            BUSINESS NAME
+                        </div>
+
+                        <div
+                            class="d-flex justify-content-center annual-header annual-business-address pb-1 mb-1 annual-data">
+                            BUSINESS ADDRESS
+                        </div>
+
+                        <div class="w-100 d-flex justify-content-between flex-gap annual-owner-wrapper">
+                            <div class=" w-50 d-flex flex-column justify-content-center align-items-center ">
+                                <div class="w-100 d-flex justify-content-center annual-owner p-1 annual-data">
+                                    OWNER NAME
+                                </div>
+                                <p class="w-100 m-0 text-center annual-owner-title">CHARACTER OF OCCUPANCY</p>
+                            </div>
+
+
+                            <div class="w-50 d-flex flex-column justify-content-center align-items-center">
+                                <div class="w-100 d-flex justify-content-center annual-group p-1 annual-data">
+                                    B
+                                </div>
+                                <p class="w-100 m-0 text-center annual-group-title">Group</p>
+                            </div>
+                        </div>
+
+                        <div class="annual-certification-description">
+                            A CERTIFICATION DULY SIGNED AND SEALED FROM A DULY LICENSED ARCHITECT/CIVIL ENGINEER,
+                            PROFESSIONAL ELECTRICAL ENGINEER/ ELECTRONICS ENGINEER/PROFESSIONAL MECHANICAL ENGINEER,
+                            MASTER PLUMBER AND SANITARY ENGINEER HIRED BY THE OWNER WAS SUBMITTED AND WHO UNDERTOOK
+                            THE
+                            ANNUAL INSPECTION THAT THE BUILDING/STRUCTURE IS ARCHITECTURALLY PRESENTABLE,
+                            STRUCTURALLY
+                            SAFE, THE ELECTRICAL
+                        </div>
+
+                        <div class="d-flex flex-column align-items-center">
+                            <div class="verified-title mb-1">
+                                VERIFIED AS TO THE FOLLOWING
+                            </div>
+
+                            <div class="verified-by-wrapper d-flex justify-content-center flex-wrap">
+
+                                <div class="verified-by-container">
+                                    <div class="verified-by-names w-100 d-flex justify-content-center annual-data">
+                                        TRY
+                                    </div>
+                                    <p class="w-100 m-0 text-center verified-by-position">LOCATIONAL/ZONING
+                                        OF LAND USE
+                                    </p>
+                                </div>
+
+                                <div class="verified-by-container">
+                                    <div class="verified-by-names w-100 d-flex justify-content-center annual-data">
+                                        TRY
+
+                                    </div>
+                                    <p class="w-100 m-0 text-center verified-by-position">LINE AND GRADE (GEODETIC)
+                                    </p>
+                                </div>
+
+                                <div class="verified-by-container">
+                                    <div class="verified-by-names w-100 d-flex justify-content-center annual-data">
+                                        TRY
+
+                                    </div>
+                                    <p class="w-100 m-0 text-center verified-by-position">ARCHITECTURAL
+                                    </p>
+                                </div>
+
+                                <div class="verified-by-container">
+                                    <div class="verified-by-names w-100 d-flex justify-content-center annual-data">
+                                        TRY
+
+                                    </div>
+                                    <p class="w-100 m-0 text-center verified-by-position">CIVIL/ STRUCTURAL
+                                    </p>
+                                </div>
+
+                                <div class="verified-by-container">
+                                    <div class="verified-by-names w-100 d-flex justify-content-center annual-data">
+                                        TRY
+
+                                    </div>
+                                    <p class="w-100 m-0 text-center verified-by-position">ELECTRICAL
+                                    </p>
+                                </div>
+
+                                <div class="verified-by-container">
+                                    <div class="verified-by-names w-100 d-flex justify-content-center annual-data">
+                                        TRY
+
+                                    </div>
+                                    <p class="w-100 m-0 text-center verified-by-position">MECHANICAL
+                                    </p>
+                                </div>
+
+                                <div class="verified-by-container">
+                                    <div class="verified-by-names w-100 d-flex justify-content-center annual-data">
+                                        TRY
+
+                                    </div>
+                                    <p class="w-100 m-0 text-center verified-by-position">SANITARY
+                                    </p>
+                                </div>
+
+                                <div class="verified-by-container">
+                                    <div class="verified-by-names w-100 d-flex justify-content-center annual-data">
+                                        TRY
+
+                                    </div>
+                                    <p class="w-100 m-0 text-center verified-by-position">PLUMBING
+                                    </p>
+                                </div>
+
+                                <div class="verified-by-container">
+                                    <div class="verified-by-names w-100 d-flex justify-content-center annual-data">
+                                        TRY
+
+                                    </div>
+                                    <p class="w-100 m-0 text-center verified-by-position">ELECTRONICS
+                                    </p>
+                                </div>
+
+                                <div class="verified-by-container">
+                                    <div class="verified-by-names w-100 d-flex justify-content-center annual-data">
+                                        TRY
+
+                                    </div>
+                                    <p class="w-100 m-0 text-center verified-by-position">INTERIOR
+                                    </p>
+                                </div>
+                                <div class="verified-by-container">
+                                    <div class="verified-by-names w-100 d-flex justify-content-center annual-data">
+                                        TRY
+
+                                    </div>
+                                    <p class="w-100 m-0 text-center verified-by-position">ACCESSIBILITY
+                                    </p>
+                                </div>
+                                <div class="verified-by-container">
+                                    <div class="verified-by-names w-100 d-flex justify-content-center annual-data">
+                                        TRYY
+                                    </div>
+                                    <p class="w-100 m-0 text-center verified-by-position">FIRE
+                                    </p>
+                                </div>
+                                <div class="verified-by-container">
+                                    <div class="verified-by-names w-100 d-flex justify-content-center annual-data">
+                                        TRY
+                                    </div>
+                                    <p class="w-100 m-0 text-center verified-by-position">OTHERS (SPECIFY)
+                                    </p>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div class="annual-recommended-description">
+                            THE ABOVE-DESCRIBED BUILDING/STRUCTURE COVERED BY CERTIFICATE OF OCCUPANCY NO. <u>N/A</u>
+                            N/A
+                            ISSUED
+                            ON <u>N/A</u> HAS BEEN VERIFIED AND FOUND SUBSTANTIALLY SATISFACTORY COMPLIED, THEREFORE THE
+                            “CERTIFICATE OF ANNUAL INSPECTION” IS HEREBY RECOMMENDED FOR ISSUANCE.
+                        </div>
+
+                        <div class="annual-footer d-flex justify-content-center">
+                            <div class="w-50 d-flex flex-column align-items-center">
+                                <div class="chief-name"><u>ENGR. GREGORY KARL D. SAN JUAN</u></div>
+                                <div class="d-flex flex-column align-items-center">
+                                    <div class="chief-title">SECTION CHIEF</div>
+                                    <div class="inspection-division-title">
+                                        INSPECTION AND ENFORCEMENT DIVISION
+                                    </div>
+                                    <div class="signature-title">
+                                        (SIGNATURE OVER PRINTED NAME)
+                                    </div>
+                                </div>
+                                <div class="annual-date d-flex justify-content-center w-100">
+                                    <p class="m-0">DATE</p> <span class="annual-date-underline">
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="w-50 d-flex flex-column align-items-center">
+                                <div class="chief-name"><u>ENGR. EMMANUEL T. AWAYAN</u></div>
+                                <div class="d-flex flex-column align-items-center">
+                                    <div class="chief-title">DIVISION CHIEF</div>
+                                    <div class="inspection-division-title">
+                                        INSPECTION AND ENFORCEMENT DIVISION
+                                    </div>
+                                    <div class="signature-title">
+                                        (SIGNATURE OVER PRINTED NAME)
+                                    </div>
+                                </div>
+                                <div class="annual-date d-flex justify-content-center w-100">
+                                    <p class="m-0">DATE</p> <span class="annual-date-underline">
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="annual-sheet-right">
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<a class=" scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<?php 
+require './../includes/footer.php'; 
+?>
+</body>
+
+</html>
