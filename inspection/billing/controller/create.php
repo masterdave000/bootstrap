8b053e23a3c6f3c153907768ae3bef23d0347891
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-04-24 00:26:14 +0800

    Add: Add Billing Fee Record

---
+++ inspection/billing/controller/create.php
@@ -0,0 +1,76 @@
+<?php
+
+include './../../../config/constants.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    $clean_category_id = filter_var($_POST['category_id'], FILTER_SANITIZE_NUMBER_INT);
+    $category_id = filter_var($clean_category_id, FILTER_VALIDATE_INT);
+    $section = $_POST['section'];
+    $capacity = $_POST['capacity'];
+    $fee = $_POST['fee'];
+
+    $fetchBilling = "SELECT section, capacity FROM equipment_billing_view WHERE category_id = :category_id AND section = :section";
+    $fetchBillingStatement = $pdo->prepare($fetchBilling);
+    $fetchBillingStatement->bindParam(':category_id', $category_id);
+    $fetchBillingStatement->bindParam(':section', $section);
+    $fetchBillingStatement->execute();
+
+    $billingRecordCount = $fetchBillingStatement->rowCount();
+    if ($billingRecordCount > 0) {
+        
+        $_SESSION['duplicate'] = "
+        <div class='msgalert alert--danger' id='alert'>
+            <div class='alert__message'>	
+                $section Fee Already Exist
+            </div>
+        </div>
+        
+        ";
+
+        header('location:' . SITEURL . 'inspection/billing/add-billing.php');
+        exit;
+    }
+    
+}
+
+$billingQuery = "INSERT INTO equipment_billing (
+    category_id,
+    section,
+    capacity,
+    fee   
+) VALUES (
+    :category_id,
+    :section,
+    :capacity,
+    :fee 
+)";
+
+
+$billingStatement = $pdo->prepare($billingQuery);
+$billingStatement->bindParam(':category_id', $category_id);
+$billingStatement->bindParam(':section', $section);
+$billingStatement->bindParam(':capacity', $capacity);
+$billingStatement->bindParam(':fee', $fee);
+
+if ($billingStatement->execute()) {
+    $_SESSION['add'] = "
+        <div class='msgalert alert--success' id='alert'>
+            <div class='alert__message'>
+                Billing Added Successfully
+            </div>
+        </div>
+    ";
+
+    header('location:' . SITEURL . 'inspection/billing/');
+} else {
+    $_SESSION['add'] = "
+        <div class='msgalert alert--danger' id='alert'>
+            <div class='alert__message'>	
+                Failed to Add Billing
+            </div>
+        </div>
+    ";
+
+    header('location:' . SITEURL . 'inspection/billing/add-billing.php');
+}
