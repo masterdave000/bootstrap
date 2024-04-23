1cf884a4b55d642a8e0304e068302327176886ce
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-04-23 23:13:17 +0800

    Edit: Directory user -> business

--- inspection/business/controller/delete.php
+++ inspection/business/controller/delete.php
@@ -21,7 +21,7 @@ if (filter_has_var(INPUT_POST, 'password')) {
 
         ";
         //Redirecting to the manage user page.
-        header('location:' . SITEURL . 'inspection/user/');
+        header('location:' . SITEURL . 'inspection/business/');
     }
     
 }
