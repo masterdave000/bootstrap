18114a695c1ae7c94a701f73aee5b3a9c13feff7
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-04-30 19:56:33 +0800

    Edit: Add jquery.easing.min.js

--- inspection/includes/footer.php
+++ inspection/includes/footer.php
@@ -1,9 +1,11 @@
 <!-- Bootstrap core JavaScript-->
+<script src="
+https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js
+"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
 </script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-
+<script src="<?php echo SITEURL ?>vendor/jquery-easing/jquery.easing.min.js"></script>
 <script src="<?php echo SITEURL ?>assets/js/main.js"></script>
 <script src="<?php echo SITEURL ?>assets/js/sb-admin-2.js"></script>
 
@@ -23,7 +25,6 @@ if ($title === 'Dashboard') :?>
 <script src="<?php echo SITEURL ?>vendor/datatables/dataTables.bootstrap4.min.js"></script>
 <!--Page level custom scripts -->
 <script src="<?php echo SITEURL ?>asets/js/demo/datatables-demo.js"></script>
-
 
 
 <?php endif;?>
