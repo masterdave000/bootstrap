a81b498ad9c5318b51bab85b657b39ba33981ad6
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-04-30 17:40:42 +0800

    Edit: Remove Local Jquery, Add Latest Jquery Link

--- inspection/includes/footer.php
+++ inspection/includes/footer.php
@@ -2,11 +2,9 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
 </script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
 <script src="<?php echo SITEURL ?>assets/js/main.js"></script>
-<script src="<?php echo SITEURL ?>vendor/jquery/jquery.min.js"></script>
-<!-- Core plugin JavaScript-->
-<script src="<?php echo SITEURL ?>vendor/jquery-easing/jquery.easing.min.js"></script>
-<!-- Custom scripts for all pages-->
 <script src="<?php echo SITEURL ?>assets/js/sb-admin-2.js"></script>
 
 
@@ -25,5 +23,7 @@ if ($title === 'Dashboard') :?>
 <script src="<?php echo SITEURL ?>vendor/datatables/dataTables.bootstrap4.min.js"></script>
 <!--Page level custom scripts -->
 <script src="<?php echo SITEURL ?>asets/js/demo/datatables-demo.js"></script>
+
+
 
 <?php endif;?>
