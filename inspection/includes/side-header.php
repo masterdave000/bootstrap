7413fb798d9ba853c72334ffed1350dbfeca4bca
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-04-30 22:04:10 +0800

    Add: Equipment Print Css

--- inspection/includes/side-header.php
+++ inspection/includes/side-header.php
@@ -32,6 +32,7 @@ $username = $_SESSION['username'];
     <link href="<?php echo SITEURL ?>assets/css/sb-admin-2.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="<?php echo SITEURL ?>assets/css/equipment-print.css" rel="stylesheet" media="print">
 </head>
 
 <body id="page-top">
