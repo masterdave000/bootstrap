152bf46426147f30aa42f49566d0c68de6ef7d6f
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-05-02 13:01:47 +0800

    Edit: Add dataTables Link and Script

--- inspection/includes/side-header.php
+++ inspection/includes/side-header.php
@@ -33,6 +33,7 @@ $username = $_SESSION['username'];
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="<?php echo SITEURL ?>assets/css/equipment-print.css" rel="stylesheet" media="print">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" />
 </head>
 
 <body id="page-top">
