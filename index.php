9a1404107161b1ecc7067fa0d146523d1f1cc274
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-05-03 15:25:52 +0800

    Edit: Remove Remember Me, Forgot Password?, and Create an Account

--- index.php
+++ index.php
@@ -75,23 +75,9 @@ require "./config/constants.php";
                                                 class="form-control form-control-user" id="exampleInputPassword"
                                                 placeholder="Password">
                                         </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
-                                            </div>
-                                        </div>
                                         <input type="submit" name="submit" value="Login"
                                             class="btn btn-primary btn-user btn-block">
                                     </form>
-                                    <hr>
-                                    <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                    </div>
-                                    <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
-                                    </div>
                                 </div>
                             </div>
                         </div>
