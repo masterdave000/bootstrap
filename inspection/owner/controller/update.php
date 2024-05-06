68d915b15386d733ccf16167fb68f00ab1596f88
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-05-07 07:38:13 +0800

    Edit: Add Checking of Owner Record Data Duplication

--- inspection/owner/controller/update.php
+++ inspection/owner/controller/update.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 include './../../../config/constants.php';
 
@@ -9,10 +9,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $owner_midname = htmlspecialchars(trim(ucwords($_POST['owner_midname'])));
     $owner_lastname = htmlspecialchars(trim(ucwords($_POST['owner_lastname'])));
     $owner_suffix = htmlspecialchars(trim(ucwords($_POST['owner_suffix'])));
+    $fullname = trim($owner_firstname . ' ' . $owner_midname . ' ' . $owner_lastname . ' ' . $owner_suffix);
+
     $contact_number = trim($_POST['contact_number']);
     $clean_email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
     $email = strtolower(trim(filter_var($clean_email, FILTER_VALIDATE_EMAIL)));
-    
+
     $img_name = basename($_FILES['owner_img_url']['name']);
     $temp_name = $_FILES['owner_img_url']['tmp_name'];
     $img_size = $_FILES['owner_img_url']['size'];
@@ -21,19 +23,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $img_extension = strtolower(pathinfo($img_name, PATHINFO_EXTENSION));
     $allowed_types = ['jpeg', 'png', 'jpg'];
     $folder_path = "./../images/";
-    $filename = $owner_lastname . '-' . $owner_firstname. '(' . date('m-d-Y') . ').png';
+    $filename = $owner_lastname . '-' . $owner_firstname . '(' . date('m-d-Y') . ').png';
 
     $owner_img_url = $_POST['current_img_url'];
-    
+
     if ($img_name) {
         if (!in_array($img_extension, $allowed_types)) {
-                $_SESSION['error'] = "Only JPEG, JPG, and PNG files are allowed.";
+            $_SESSION['error'] = "Only JPEG, JPG, and PNG files are allowed.";
         } elseif ($img_size >= $max_size) {
-                $_SESSION['error'] = "File size must be less than 1MB.";
+            $_SESSION['error'] = "File size must be less than 1MB.";
         } else {
-                $upload = $filename;
-                move_uploaded_file($temp_name, $folder_path . $upload);
-                $owner_img_url = $upload;
+            $upload = $filename;
+            move_uploaded_file($temp_name, $folder_path . $upload);
+            $owner_img_url = $upload;
         }
 
         // Redirect to the form file with an error message
@@ -41,8 +43,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             header("Location: ../update-owner.php?owner_id='$owner_id'");
             exit();
         }
-
     }
+}
+
+$ownerDuplicate = "SELECT owner_id FROM owner WHERE owner_firstname = :owner_firstname AND owner_midname = :owner_midname AND owner_lastname = :owner_lastname AND owner_suffix = :owner_suffix";
+$ownerStatement = $pdo->prepare($ownerDuplicate);
+$ownerStatement->bindParam(':owner_firstname', $owner_firstname);
+$ownerStatement->bindParam(':owner_midname', $owner_midname);
+$ownerStatement->bindParam(':owner_lastname', $owner_lastname);
+$ownerStatement->bindParam(':owner_suffix', $owner_suffix);
+$ownerStatement->execute();
+
+$ownerCount = $ownerStatement->rowCount();
+
+if ($ownerCount > 0) {
+    $_SESSION['duplicate'] = "
+    <div class='msgalert alert--danger' id='alert'>
+        <div class='alert__message'>	
+            $fullname Record Already Exist
+        </div>
+    </div>
+    
+    ";
+
+    header('location:' . SITEURL . "inspection/owner/update-owner.php?owner_id=$owner_id");
+    exit;
 }
 
 $updateOwnerQuery = "UPDATE owner SET
