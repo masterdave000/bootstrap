60a9e2fe1d4e8facf3df59ee71153da4027da1f3
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-05-08 19:36:47 +0800

    Edit: Add data-item-id to Select Button ?>

--- inspection/inspection/modals/item.php
+++ inspection/inspection/modals/item.php
@@ -1,6 +1,5 @@
 <!-- Logout Modal-->
-<div class="modal fade" id="item-list" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
+<div class="modal fade" id="item-list" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -15,44 +14,42 @@
                     <thead>
                     </thead>
                     <tbody class="w-100">
-                        <?php 
-                                $itemQuery = "SELECT * FROM item_view ORDER BY item_name";
-                                $itemStatement = $pdo->query($itemQuery);
-                                $items = $itemStatement->fetchAll(PDO::FETCH_ASSOC);
-                                foreach ($items as $item) {
-                            ?>
-                        <tr class="d-flex justify-content-between align-items-center border-bottom py-1 select-item"
-                            data-item-id="<?php echo $item['item_id']?>">
-                            <td class="p-0 m-0 w-100">
-                                <a href="#" class="d-flex align-items-center text-decoration-none
-                                text-gray-700 flex-gap w-100">
-                                    <div class=" image-container img-fluid">
-                                        <img src="./../item/images/<?php echo $item['img_url'] ?? 'default-img.png'?>"
-                                            alt="inspector-image" class="img-fluid rounded-circle" />
-                                    </div>
-
-                                    <div>
-                                        <div class="text">
-                                            <span class="d-none d-md-inline">Name:</span>
-                                            <?php echo $item['item_name']?>
-                                        </div>
-                                        <div class="sub-title d-none d-md-flex">
-                                            <?php echo $item['category_name']?></div>
-
-                                    </div>
-                                </a>
-
-                            </td>
-                            <td>
-                                <a class="btn btn-primary py-1 select-item">
-                                    Select
-                                </a>
-                            </td>
-                        </tr>
                         <?php
-                                }
+                        $itemQuery = "SELECT * FROM item_view ORDER BY item_name";
+                        $itemStatement = $pdo->query($itemQuery);
+                        $items = $itemStatement->fetchAll(PDO::FETCH_ASSOC);
+                        foreach ($items as $item) {
+                        ?>
+                            <tr class="d-flex justify-content-between align-items-center border-bottom py-1 select-item" data-item-id="<?php echo $item['item_id'] ?>">
+                                <td class="p-0 m-0 w-100">
+                                    <a href="#" class="d-flex align-items-center text-decoration-none
+                                text-gray-700 flex-gap w-100">
+                                        <div class=" image-container img-fluid">
+                                            <img src="./../item/images/<?php echo $item['img_url'] ?? 'default-img.png' ?>" alt="inspector-image" class="img-fluid rounded-circle" />
+                                        </div>
 
-                            ?>
+                                        <div>
+                                            <div class="text">
+                                                <span class="d-none d-md-inline">Name:</span>
+                                                <?php echo $item['item_name'] ?>
+                                            </div>
+                                            <div class="sub-title d-none d-md-flex">
+                                                <?php echo $item['category_name'] ?></div>
+
+                                        </div>
+                                    </a>
+
+                                </td>
+                                <td>
+                                    <a class="btn btn-primary py-1" data-item-id="<?php echo $item['item_id'] ?>">
+                                        Select
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php
+                        }
+
+                        ?>
 
                     </tbody>
                 </table>
