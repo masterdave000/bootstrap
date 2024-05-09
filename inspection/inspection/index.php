4e62e6106f411eb2f0321904c9e4ebf04471628f
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-05-09 21:36:32 +0800

    Edit: Add Reissue Success Message

--- inspection/inspection/index.php
+++ inspection/inspection/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 $title = "Inspection List";
 require "./../includes/side-header.php";
@@ -9,15 +9,14 @@ require "./../includes/side-header.php";
     <!-- Main Content -->
     <div id="content">
 
-        <?php require './../includes/top-header.php'?>
+        <?php require './../includes/top-header.php' ?>
 
         <!-- Begin Page Content -->
         <div class="container-fluid mt-4">
             <div class="card shadow mb-4">
                 <div class="d-flex align-items-center justify-content-between card-header h-75">
                     <h1 class="h3 text-gray-800 mt-2"><?php echo $title ?></h1>
-                    <a href="./add-inspection.php"
-                        class="btn btn-primary d-flex justify-content-center align-items-center">
+                    <a href="./add-inspection.php" class="btn btn-primary d-flex justify-content-center align-items-center">
                         <i class="fa fa-plus mr-1" aria-hidden="true"></i>
                         <span class="d-none d-lg-inline">Issue</span>
                     </a>
@@ -36,42 +35,40 @@ require "./../includes/side-header.php";
 
                             <tbody>
 
-                                <?php 
-                                    $inspectionQuery = "SELECT DISTINCT inspection_id, bus_name, bus_img_url, date_inspected FROM inspection_view ORDER BY inspection_id DESC";
-                                    $inspectionStatement = $pdo->query($inspectionQuery);
-                                    $inspections = $inspectionStatement->fetchAll(PDO::FETCH_ASSOC);
-                                    ?>
-                                <?php 
-                            
-                                    foreach ($inspections as $inspection) :
+                                <?php
+                                $inspectionQuery = "SELECT DISTINCT inspection_id, bus_name, bus_img_url, date_inspected FROM inspection_view ORDER BY inspection_id DESC";
+                                $inspectionStatement = $pdo->query($inspectionQuery);
+                                $inspections = $inspectionStatement->fetchAll(PDO::FETCH_ASSOC);
+                                ?>
+                                <?php
+
+                                foreach ($inspections as $inspection) :
                                 ?>
 
 
-                                <tr class="d-flex justify-content-between align-items-center border-bottom py-1">
-                                    <td class="p-0 m-0">
-                                        <a href="./view-inspection.php?inspection_id=<?php echo $inspection['inspection_id']?>"
-                                            class="d-flex flex-row align-items-center justify-content-center text-decoration-none
+                                    <tr class="d-flex justify-content-between align-items-center border-bottom py-1">
+                                        <td class="p-0 m-0">
+                                            <a href="./view-inspection.php?inspection_id=<?php echo $inspection['inspection_id'] ?>" class="d-flex flex-row align-items-center justify-content-center text-decoration-none
                                 text-gray-700 flex-gap">
-                                            <div class="image-container img-fluid">
-                                                <img src="./../business/images/<?php echo $inspection['bus_img_url'] ?? 'no-image.png'?>"
-                                                    alt="inspector-image" class="img-fluid rounded-circle" />
-                                            </div>
-
-                                            <div>
-                                                <div class="text">
-                                                    <?php echo $inspection['bus_name']?>
+                                                <div class="image-container img-fluid">
+                                                    <img src="./../business/images/<?php echo $inspection['bus_img_url'] ?? 'no-image.png' ?>" alt="inspector-image" class="img-fluid rounded-circle" />
                                                 </div>
-                                                <div class="sub-title d-none d-md-flex">Date Inspected:
-                                                    <?php echo $inspection['date_inspected']?></div>
 
-                                            </div>
-                                        </a>
-                                    </td>
-                                </tr>
+                                                <div>
+                                                    <div class="text">
+                                                        <?php echo $inspection['bus_name'] ?>
+                                                    </div>
+                                                    <div class="sub-title d-none d-md-flex">Date Inspected:
+                                                        <?php echo $inspection['date_inspected'] ?></div>
+
+                                                </div>
+                                            </a>
+                                        </td>
+                                    </tr>
 
                                 <?php
-                                   
-                                    endforeach;
+
+                                endforeach;
                                 ?>
 
                             </tbody>
@@ -83,22 +80,35 @@ require "./../includes/side-header.php";
     </div>
 </div>
 
-<?php 
-        
-    if (isset($_SESSION['add'])) { //Checking whether the session is set or not
-	//DIsplaying session message
-        echo $_SESSION['add'];
-        //Removing session message
-        unset($_SESSION['add']);
-    }
+<?php
+
+if (isset($_SESSION['add'])) { //Checking whether the session is set or not
+    //DIsplaying session message
+    echo $_SESSION['add'];
+    //Removing session message
+    unset($_SESSION['add']);
+}
+
+
 
 ?>
+
+<div class='msgalert alert--success' id='alert'>
+    <div class='alert__message'>
+        <?php
+        $msg = htmlspecialchars($_GET['msg']);
+        unset($_GET['msg']);
+        echo $msg;
+        ?>
+    </div>
+</div>
+
 <!-- Scroll to Top Button-->
 <a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
 </a>
 
-<?php 
+<?php
 require './../includes/footer.php';
 ?>
 
