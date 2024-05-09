3aee5be7217a4b2bf38ffd9a11f1f88753c70d34
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-05-09 16:41:42 +0800

    Edit: Fetch Fee electronics_billing -> equipment_billing_view

--- inspection/inspection/json_response/electronics-fee.php
+++ inspection/inspection/json_response/electronics-fee.php
@@ -6,7 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Fetch sections from the database based on the selected category
     // Modify this query according to your database structure
-    $feeQuery = "SELECT electronics_id, electronics_fee FROM electronics_billing WHERE electronics_section = :section";
+    $feeQuery = "SELECT billing_id, fee FROM equipment_billing_view WHERE section = :section";
 
     // Prepare and execute the query
     $feeStatement = $pdo->prepare($feeQuery);
@@ -16,23 +16,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Fetch the capacities
     $fee = $feeStatement->fetch(PDO::FETCH_ASSOC);
 
+    $billing_id = $fee['billing_id'];
     // Prepare the JSON response
     $response = array(
-        'electronics_id' => $fee['electronics_id'],
-        'electronics_fee' => $fee['electronics_fee']
+        'billing_id' => $billing_id,
+        'fee' => $fee['fee']
     );
+
 
     // Set the content type header
     header('Content-Type: application/json');
-
     // Send the JSON response
     echo json_encode($response);
 } else {
     // Handle the case when section is not set
     // You can return an error response or an empty array
     $response = array(
-        'electronics_id' => '',
-        'electronics_fee' => 0.00
+        'billing_id' => '',
+        'fee' => 0.00
     );
 
     header('Content-Type: application/json');
