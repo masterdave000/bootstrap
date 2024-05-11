26bb8072332c1a3a720eae1795a1195ce8ddd2a6
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-05-11 10:14:32 +0800

    Edit: Add font awesome CDN link

--- inspection/includes/side-header.php
+++ inspection/includes/side-header.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require __DIR__ . "/../../config/constants.php";
 require __DIR__ . "/../../login-check.php";
 
@@ -21,25 +21,22 @@ $role = $_SESSION['role'];
     <title><?php echo $title ?></title>
 
     <!-- Custom fonts for this template-->
-    <link rel="icon" type="image/x-icon" href="<?php echo SITEURL?>assets/img/lgu_logo.ico">
-    <link href="<?php echo SITEURL?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="<?php echo SITEURL ?>assets/img/lgu_logo.ico">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="<?php echo SITEURL ?>assets/css/style.css" rel="stylesheet">
     <link href="<?php echo SITEURL ?>assets/css/sb-admin-2.min.css" rel="stylesheet">
     <link href="<?php echo SITEURL ?>assets/css/sb-admin-2.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <?php if ($title === 'Equipment List Certificate'): ?>
-    <link href="<?php echo SITEURL ?>assets/css/equipment-print.css" rel="stylesheet" media="print">
+    <?php if ($title === 'Equipment List Certificate') : ?>
+        <link href="<?php echo SITEURL ?>assets/css/equipment-print.css" rel="stylesheet" media="print">
     <?php endif ?>
 
-    <?php if ($title === 'Annual Certificate'): ?>
-    <link href="<?php echo SITEURL ?>assets/css/certificate-print.css" rel="stylesheet" media="print">
+    <?php if ($title === 'Annual Certificate') : ?>
+        <link href="<?php echo SITEURL ?>assets/css/certificate-print.css" rel="stylesheet" media="print">
     <?php endif ?>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" />
 </head>
@@ -52,8 +49,7 @@ $role = $_SESSION['role'];
         <ul class="navbar-nav bg-sidebar-primary sidebar sidebar-dark accordion d-print-none" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center"
-                href="<?php echo SITEURL; ?>inspection/dashboard/">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?php echo SITEURL; ?>inspection/dashboard/">
                 <div class="sidebar-brand-icon rotate-n-15" style="width: 50px; height: 50px">
                     <img style="width: 100%; height: 100%" src="<?php echo SITEURL ?>assets/img/lgu_logo.png">
                 </div>
@@ -68,72 +64,72 @@ $role = $_SESSION['role'];
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo SITEURL?>inspection/category/">
+                <a class="nav-link" href="<?php echo SITEURL ?>inspection/category/">
                     <i class="fas fa-fw fa-list"></i>
                     <span>Category</span></a>
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo SITEURL?>inspection/item/">
+                <a class="nav-link" href="<?php echo SITEURL ?>inspection/item/">
                     <i class="fas fa-fw fa-list"></i>
                     <span>Item</span></a>
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo SITEURL?>inspection/inspection/">
+                <a class="nav-link" href="<?php echo SITEURL ?>inspection/inspection/">
                     <i class="fas fa-fw fa-check-square"></i>
                     <span>Inspection</span></a>
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo SITEURL?>inspection/certificate/">
+                <a class="nav-link" href="<?php echo SITEURL ?>inspection/certificate/">
                     <i class="fas fa-fw fa-file-medical"></i>
                     <span>Certificate</span></a>
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo SITEURL?>inspection/business/">
+                <a class="nav-link" href="<?php echo SITEURL ?>inspection/business/">
                     <i class="fas fa-fw fa-business-time"></i>
                     <span>Business</span></a>
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo SITEURL?>inspection/owner/">
+                <a class="nav-link" href="<?php echo SITEURL ?>inspection/owner/">
                     <i class="fas fa-fw fa-user"></i>
                     <span>Owner</span></a>
             </li>
 
-            <?php if ($role === 'Administrator'): ?>
+            <?php if ($role === 'Administrator') : ?>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo SITEURL ?>inspection/inspector/">
+                        <i class="fas fa-fw fa-users"></i>
+                        <span>Inspector</span></a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo SITEURL ?>inspection/billing/">
+                        <i class="fa fa-credit-card-alt"></i>
+                        <span>Billing</span></a>
+                </li>
+
+            <?php endif; ?>
 
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo SITEURL?>inspection/inspector/">
-                    <i class="fas fa-fw fa-users"></i>
-                    <span>Inspector</span></a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo SITEURL?>inspection/billing/">
-                    <i class="fa fa-credit-card-alt"></i>
-                    <span>Billing</span></a>
-            </li>
-
-            <?php endif;?>
-
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo SITEURL?>inspection/violation/">
-                    <i class="fas fa-fw fa-warning"></i>
+                <a class="nav-link" href="<?php echo SITEURL ?>inspection/violation/">
+                    <i class="fa-solid fa-fw fa-triangle-exclamation"></i>
                     <span>Violation</span></a>
             </li>
 
-            <?php if ($role === 'Administrator'): ?>
+            <?php if ($role === 'Administrator') : ?>
 
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo SITEURL?>inspection/user/">
-                    <i class="fas fa-fw fa-circle-user"></i>
-                    <span>User</span></a>
-            </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo SITEURL ?>inspection/user/">
+                        <i class="fas fa-fw fa-circle-user"></i>
+                        <span>User</span></a>
+                </li>
 
-            <?php endif;?>
+            <?php endif; ?>
 
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
