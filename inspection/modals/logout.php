33ff542b9bab89a23128d38fe3d9ba1ac381fe6f
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-05-12 20:43:42 +0800

    Edit: Add SITEURL to href

--- inspection/modals/logout.php
+++ inspection/modals/logout.php
@@ -1,6 +1,5 @@
 <!-- Logout Modal-->
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -12,7 +11,7 @@
             <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
             <div class="modal-footer">
                 <button class="btn btn-dark" type="button" data-bs-dismiss="modal">Cancel</button>
-                <a class="btn btn-danger" href="./../../logout.php">Logout</a>
+                <a class="btn btn-danger" href="<?php echo SITEURL ?>logout.php">Logout</a>
             </div>
         </div>
     </div>
