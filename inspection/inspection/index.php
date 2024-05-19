5c3f09f97f695c2233df38e983a9b4c3cd723c9b
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-05-19 13:05:46 +0800

    Edit: Link Issue Button to Busines Modal

--- inspection/inspection/index.php
+++ inspection/inspection/index.php
@@ -16,7 +16,7 @@ require "./../includes/side-header.php";
             <div class="card shadow mb-4">
                 <div class="d-flex align-items-center justify-content-between card-header h-75">
                     <h1 class="h3 text-gray-800 mt-2"><?php echo $title ?></h1>
-                    <a href="./add-inspection.php" class="btn btn-primary d-flex justify-content-center align-items-center">
+                    <a href="#" data-bs-toggle="modal" data-bs-target="#business-list" class="btn btn-primary d-flex justify-content-center align-items-center">
                         <i class="fa fa-plus mr-1" aria-hidden="true"></i>
                         <span class="d-none d-lg-inline">Issue</span>
                     </a>
@@ -79,6 +79,11 @@ require "./../includes/side-header.php";
         </div>
     </div>
 </div>
+
+<?php
+
+include "./modals/business.php";
+?>
 
 <?php
 
